9ac2946cd63bdd51a71f81d78fcc6a6b809d6496
Pablo Johnson <pablo.88j@gmail.com>
2017-02-07 16:55:54 -0500

    samples

---
+++ BuildVariant/app/src/full/java/com/johnson/pablo/buildvariant/activities/MainActivity.java
@@ -0,0 +1,51 @@
+package com.johnson.pablo.buildvariant.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.johnson.pablo.buildvariant.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REGISTER_REQUEST_CODE = 2020;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, SecondActivity.class), REGISTER_REQUEST_CODE);
+            }
+        });
+    }
+
+    /**
+     * Dispatch incoming result to the correct fragment.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REGISTER_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Toast.makeText(this, R.string.thanksMessage,Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ BuildVariant/app/src/full/java/com/johnson/pablo/buildvariant/activities/SecondActivity.java
@@ -0,0 +1,46 @@
+package com.johnson.pablo.buildvariant.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.johnson.pablo.buildvariant.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.register_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.registerAction:
+                setResult(Activity.RESULT_OK);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
