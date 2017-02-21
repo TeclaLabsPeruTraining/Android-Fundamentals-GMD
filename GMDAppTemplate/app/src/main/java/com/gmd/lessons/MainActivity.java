56af3e44ce88dd1c2adcbbcf4fa0f6394ff64727
Pablo Johnson <pablo.88j@gmail.com>
2017-02-21 15:35:08 -0500

    simple transition animation and ripple

--- GMDAppTemplate/app/src/main/java/com/gmd/lessons/MainActivity.java
+++ GMDAppTemplate/app/src/main/java/com/gmd/lessons/MainActivity.java
@@ -1,13 +1,30 @@
 package com.gmd.lessons;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // habilitar animaciones
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWindow().setExitTransition(new Explode());
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent,
+                        ActivityOptions
+                                .makeSceneTransitionAnimation(MainActivity.this).toBundle());
+            }
+        });
     }
 }
